20c55effbc35250f0ad84675bc500b09e1030428
PeterQuill <jesuse2409@gmail.com>
2019-07-08 16:41:24 -0500

    Limpieza

--- PruebaString/Program.cs
+++ PruebaString/Program.cs
@@ -11,46 +11,12 @@ namespace PruebaString
         static void Main(string[] args)
         {
 
-            //objeto a = new objeto();
-
-           // a.nombre = "Jesus";
-            //Console.WriteLine("STRING " +  a.nombre + " -  "+ a.apellido +" <--" + " --> "+ a.codigo+ " <--- "); 
-
-           /* int[] numeros = { 8, 8, 8, 8, 8, 6, 6, 6, 6, 6, 6 };
-
-            var lowNums = (from num in numeros
-                          select num).Distinct();
-
-            var todos = (from num in numeros
-                         select num);
-
-            Console.WriteLine(" CON  DISTINCT ");
-
-            // Execute the query.
-            foreach (int i in lowNums)
-            {
-                Console.Write(i + " ");
-            }
-
-            Console.WriteLine(" SIN DISTINCT ");
-
-            foreach (int i in todos)
-            {
-                Console.Write(i + " ");
-            }
-
-            Console.ReadKey();*/
             StringBuilder strBTextoLog = new StringBuilder();
 
             string limite = "Por el Norte: hasta Los Ejidos, incluyendo la Urb. Santa PorelMaría del Pinar. Por el Sur: hasta Urb. Miraflores y Av. Progreso de Castilla. Por el Este: hasta AA.HH. San Pedro y Av. Circunvalación Por el Oeste: hasta Av. Prolongación Grau Cdra. 35.";
 
             string l1;
             string l2;
-
-           // strBTextoLog.AppendLine(string.Concat("MAYUSCULAS: ", "-----------------"));
-          //  strBTextoLog.AppendLine(string.Concat("", limite.ToUpper()));
-            //limite = limite.ToUpper();
-          //  strBTextoLog.AppendLine(string.Concat("-----------------: ", "-----------------"));
 
             if (limite.Contains("Por el"))
             {
